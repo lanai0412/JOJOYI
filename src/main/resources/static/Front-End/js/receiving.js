7277a247c97071ca53ceae057a8ade117f30ce94
wenxinyan <81321711@qq.com>
2019-07-03 15:36:30 +0800

    add a

--- src/main/resources/static/Front-End/js/receiving.js
+++ src/main/resources/static/Front-End/js/receiving.js
@@ -27,17 +27,33 @@ function getAllProduct(id) {
         success: function (req) {
             console.log(req);
             for (var i = 0; i < req.length; i++) {
-            $("#tbodys").append()
+            $("#tbodys").append("<tr>" +
+                "<td>" +
+                "<p class='receivingImg'>" +
+                "<img src='" + req[i].productUrl + "'>" +
+                "</p>" +
+                "<p>" +
+                req[i].productNum +
+                "</p>" +
+                "</td>" +
+                "<td>" +
+                "单价" +
+                "</td>" +
+                "<td>" +
+                "单价" +
+                "</td>" +
+                "<td>" +
+                "单价" +
+                "</td>" +
+                "<td>" +
+                "<button type='button' class='btn btn-info'>确认收货</button>" +
+                "</td>" +
+                "</tr>")
             }
         },
         error: function () {
             console.log("请求出错")
         }
     })
-}
 
-function ifStata(stata) {
-    if(stata!=0){
-
-    }
 }
